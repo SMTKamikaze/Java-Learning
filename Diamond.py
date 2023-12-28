423ba74077ecb7ad899b4f6d1e4c66b818d8ac52
Kamikaze <varunme252@gmail.com>
2023-12-28 16:23:25 +0530

    half way

--- Diamond.py
+++ Diamond.py
@@ -1,1 +1,13 @@
-testing repo
+n=int(4321234)
+d=n
+a=int(1)
+while a<=4:
+    d=n
+    while int(d>0):
+        if int(d%10)==a:
+            print(a, end="")
+        else:
+            print(" ",end="")   
+        d=d/10
+    a+=1 
+    print("")
