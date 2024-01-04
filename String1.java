43db89fb6f979e74a22c996ef05c2bc5f27a58d8
SMTKamikaze <151499713+SMTKamikaze@users.noreply.github.com>
2024-01-04 16:52:52 +0000

    String1
    s

--- String1.java
+++ String1.java
@@ -1,20 +1,31 @@
 
 import java.util.Scanner;
-class abc
-{
-    // Write a program in Java to accept a name(Containing three words) and display only the initials (i.e., first letter of each word).
-// Sample Input: LAL KRISHNA ADVANI
-// Sample Output: L K A
-    public static void main(String[] args) 
-    {
-        Scanner sc=new Scanner(System.in);
-        String name=sc.nextLine();
-        int i=0;
 
-        for(i=0;i<name.length();i++)
-        {
-            char ch=name.charAt(i);
-            //On Hold
+class abc {
+    // Write a program in Java to accept a name(Containing three words) and display
+    // only the initials (i.e., first letter of each word).
+    // Sample Input: LAL KRISHNA ADVANI
+    // Sample Output: L K ADANI
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        String name = sc.nextLine();
+        name=name.toUpperCase();
+        int i = 0;
+        String word = "";
+        String initial = "";
+
+        for (i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if (ch != ' ')
+                word = word + ch;
+            if (ch == ' ') {
+                initial = initial + " " + word.charAt(0);
+                word = "";
+            }
         }
+        initial = initial + " " + word;
+        initial = initial.trim();
+        System.out.println(initial);
     }
 }
